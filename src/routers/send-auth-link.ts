1ec5da452a7bfbecea639c5c60d9af851a4c4f8f
Wallace Júnior <wallace.junior@qodeless.io>
2024-03-02 20:12:51 -0300

    Envio de E-mail com Nodemailer

--- src/routers/send-auth-link.ts
+++ src/routers/send-auth-link.ts
@@ -2,16 +2,22 @@ import { createId } from "@paralleldrive/cuid2";
 import chalk from "chalk";
 import { eq } from "drizzle-orm";
 import { t } from "elysia";
+import nodemailer from "nodemailer";
 import { database } from "../database";
 import { authLinks, users } from "../database/schema";
 import { elysia } from "../elysia";
 import { env } from "../env";
+import { mail } from "../lib/mail";
 
 export const sendAuthLinkRouter = elysia.post(
   "/authenticate",
   async ({ body, request, set }) => {
     const user = await database.query.users.findFirst({
       where: eq(users.email, body.email),
+      columns: {
+        id: true,
+        email: true,
+      },
     });
 
     if (!user) {
@@ -26,7 +32,18 @@ export const sendAuthLinkRouter = elysia.post(
     const authLink = new URL("/auth-links/authenticate", request.url);
     authLink.searchParams.set("code", authLinkCode);
     authLink.searchParams.set("redirect", env.AUTH_REDIRECT_URL);
-    console.log(chalk.blue.bold(authLink.toString()));
+
+    const info = await mail.sendMail({
+      from: {
+        name: "Pizza Shop",
+        address: "hi@pizzashop.com",
+      },
+      to: user.email,
+      subject: "Authenticate to Pizza Shop",
+      text: `Use the following link to authenticate on Pizza Shop: ${authLink.toString()}`,
+    });
+
+    console.log(chalk.blue.bold(nodemailer.getTestMessageUrl(info)));
     set.status = 204;
   },
   {
