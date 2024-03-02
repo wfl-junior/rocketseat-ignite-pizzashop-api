1f54e093411a182978b123da7325323ba14bf5e8
Wallace Júnior <wallace.junior@qodeless.io>
2024-03-02 14:38:42 -0300

    Relacionamentos no Drizzle

--- src/database/schema/restaurants.ts
+++ src/database/schema/restaurants.ts
@@ -1,5 +1,7 @@
 import { createId } from "@paralleldrive/cuid2";
+import { relations } from "drizzle-orm";
 import { char, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { users } from ".";
 
 export const restaurants = pgTable("restaurants", {
   id: char("id", { length: 24 }).$defaultFn(createId).primaryKey(),
@@ -12,9 +14,21 @@ export const restaurants = pgTable("restaurants", {
     .defaultNow()
     .$defaultFn(() => new Date()),
 
+  managerId: char("manager_id", { length: 24 }).references(() => users.id, {
+    onDelete: "set null",
+  }),
+
   name: varchar("name").notNull(),
   description: text("description"),
 });
+
+export const restaurantsRelations = relations(restaurants, ({ one }) => ({
+  manager: one(users, {
+    fields: [restaurants.managerId],
+    references: [users.id],
+    relationName: "restaurant_manager",
+  }),
+}));
 
 export type Restaurant = typeof restaurants.$inferSelect;
 export type NewRestaurant = typeof restaurants.$inferSelect;
