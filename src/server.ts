52ecb9e2fe8f4952347dffc6f8cff6f9b5333ad7
Wallace Júnior <wallace.junior@qodeless.io>
2024-03-02 15:36:31 -0300

    Tipagem na entrada de dados

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,49 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { database } from "./database";
 import { restaurants, users } from "./database/schema";
 import { env } from "./env";
 
-const app = new Elysia().post("/restaurants", async ({ body, set }) => {
-  const restaurant = await database.transaction(async transaction => {
-    const [manager] = await transaction
-      .insert(users)
-      .values({
-        name: body.name,
-        email: body.email,
-        phone: body.phone,
-        role: "manager",
-      })
-      .returning({ id: users.id });
+const app = new Elysia().post(
+  "/restaurants",
+  async ({ body, set }) => {
+    const restaurant = await database.transaction(async transaction => {
+      const [manager] = await transaction
+        .insert(users)
+        .values({
+          name: body.manager.name,
+          email: body.manager.email,
+          phone: body.manager.phone,
+          role: "manager",
+        })
+        .returning({ id: users.id });
 
-    const [restaurant] = await transaction
-      .insert(restaurants)
-      .values({
-        managerId: manager.id,
-        name: body.restaurantName,
-      })
-      .returning();
+      const [restaurant] = await transaction
+        .insert(restaurants)
+        .values({
+          managerId: manager.id,
+          name: body.name,
+        })
+        .returning();
 
+      return restaurant;
+    });
+
+    set.status = 201;
     return restaurant;
-  });
-
-  set.status = 201;
-  return restaurant;
-});
+  },
+  {
+    type: "application/json",
+    body: t.Object({
+      name: t.String(),
+      manager: t.Object({
+        name: t.String(),
+        email: t.String({ format: "email" }),
+        phone: t.String(),
+      }),
+    }),
+  },
+);
 
 app.listen(env.PORT, server => {
-  console.log(`HTTP server running at http://localhost:${server.port}`);
+  console.log(`HTTP server running at http://localhost:${server.port} 🚀`);
 });
